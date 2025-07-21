92d3e5888e976ae2b5bb79736308be749f160132
Alberto Zaragoza <alberzh@gmail.com>
2025-07-21 10:37:43 +0200

    Ejercicio ex00 listo.

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,11 +6,32 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 13:44:47 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/20 13:57:49 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/21 10:35:55 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strcpy(char *dest, char *src)
 {
-    .
+    int i;
+
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    char src[] = "Hola Mundo!";
+    char dest[50];
+
+    ft_strcpy(dest, src);
+    printf("String copiado: %s\n", dest);
+    return 0;
 }
