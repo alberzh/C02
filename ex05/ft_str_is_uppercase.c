062e3a1e37cf45b08939149186fb457b72c43b55
Alberto Zaragoza <alberzh@gmail.com>
2025-07-22 17:03:05 +0200

    Ejercicio ex05 listo.

--- ex05/ft_str_is_uppercase.c
+++ ex05/ft_str_is_uppercase.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 16:28:42 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/22 17:03:05 by azaragoz         ###   ########.fr       */
+/*   Created: 2025/07/22 16:58:16 by azaragoz          #+#    #+#             */
+/*   Updated: 2025/07/22 17:02:43 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	is_lowercase(char c)
+int	is_uppercase(char c)
 {
-	return (c >= 'a' && c <= 'z');
+	return (c >= 'A' && c <= 'Z');
 }
 
-int	ft_str_is_lowercase(char *str)
+int	ft_str_is_uppercase(char *str)
 {
 	int	i;
 
@@ -26,7 +26,7 @@ int	ft_str_is_lowercase(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (!is_lowercase(str[i]))
+		if (!is_uppercase(str[i]))
 			return (0);
 		i++;
 	}
@@ -36,20 +36,20 @@ int	ft_str_is_lowercase(char *str)
 /* int main(void)
 {
 	char	str1[] = "HelloWorld";
-	char	str2[] = "helloworld";
+	char	str2[] = "HELLOWORLD";
 	char    str3[] = "hello world";
 
-	if (ft_str_is_lowercase(str1))
-		printf("'%s' is lower case.\n", str1);
+	if (ft_str_is_uppercase(str1))
+		printf("'%s' is upper case.\n", str1);
 	else
-		printf("'%s' is not lower case.\n", str1);
-	if (ft_str_is_lowercase(str2))
-		printf("'%s' is lower case.\n", str2);
+		printf("'%s' is not upper case.\n", str1);
+	if (ft_str_is_uppercase(str2))
+		printf("'%s' is upper case.\n", str2);
 	else
-		printf("'%s' is not lower case.\n", str2);
-	if (ft_str_is_lowercase(str3))
-		printf("'%s' is lower case.\n", str3);
+		printf("'%s' is not upper case.\n", str2);
+	if (ft_str_is_uppercase(str3))
+		printf("'%s' is upper case.\n", str3);
 	else
-		printf("'%s' is not lower case.\n", str3);
+		printf("'%s' is not upper case.\n", str3);
 	return (0);
 } */
