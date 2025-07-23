2810521df03e913496de01d3cc53374a79b0527f
Alberto Zaragoza <alberzh@gmail.com>
2025-07-23 18:17:08 +0200

    Corrección ejercicio ex06.

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -6,37 +6,36 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:10:26 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/22 17:26:49 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:16:45 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	is_printable(char c)
-{
-	return (c >= 32 && c <= 126);
-}
-
 int	ft_str_is_printable(char *str)
 {
 	int	i;
 
+	if (!str)
+		return (1);
 	if (str[0] == '\0')
 		return (0);
 	i = 0;
 	while (str[i])
 	{
-		if (!is_printable(str[i]))
+		if (str[i] < 32 || str[i] > 126)
 			return (0);
 		i++;
 	}
 	return (1);
 }
+
 /* int main(void)
 {
 	char	str1[] = "Hello\7World";
 	char	str2[] = "HELLO\x01WORLD";
 	char    str3[] = "hello world";
+	char	str4[] = "";
 
 	if (ft_str_is_printable(str1))
 		printf("'%s' is printable.\n", str1);
@@ -48,6 +47,10 @@ int	ft_str_is_printable(char *str)
 		printf("Some character is not printable.\n");
 	if (ft_str_is_printable(str3))
 		printf("'%s' is printable.\n", str3);
+	else
+		printf("Some character is not printable.\n");
+	if (ft_str_is_printable(str4))
+		printf("'%s' is printable.\n", str4);
 	else
 		printf("Some character is not printable.\n");
 	return (0);
