902bff3f02c25d36aaa7595ce7e93a0f7638b340
Alberto Zaragoza <alberzh@gmail.com>
2025-07-23 18:09:25 +0200

    Corrección ejercicio ex05.

--- ex05/ft_str_is_uppercase.c
+++ ex05/ft_str_is_uppercase.c
@@ -6,38 +6,36 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:58:16 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/22 17:02:43 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:08:57 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	is_uppercase(char c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
-
 int	ft_str_is_uppercase(char *str)
 {
 	int	i;
 
+	if (!str)
+		return (1);
 	if (str[0] == '\0')
 		return (0);
 	i = 0;
 	while (str[i])
 	{
-		if (!is_uppercase(str[i]))
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			i++;
+		else
 			return (0);
-		i++;
 	}
 	return (1);
 }
-
 /* int main(void)
 {
 	char	str1[] = "HelloWorld";
 	char	str2[] = "HELLOWORLD";
 	char    str3[] = "hello world";
+	char	str4[] = "";
 
 	if (ft_str_is_uppercase(str1))
 		printf("'%s' is upper case.\n", str1);
@@ -51,5 +49,9 @@ int	ft_str_is_uppercase(char *str)
 		printf("'%s' is upper case.\n", str3);
 	else
 		printf("'%s' is not upper case.\n", str3);
+	if (ft_str_is_uppercase(str4))
+		printf("'%s' is upper case.\n", str4);
+	else
+		printf("'%s' is not upper case.\n", str4);
 	return (0);
 } */
