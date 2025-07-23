a6b0f7694706c58ac11117ff7c0d69b4c761a99d
Alberto Zaragoza <alberzh@gmail.com>
2025-07-23 18:38:33 +0200

    Ejercicio ex07 Listo.

---
+++ ex07/ft_strupcase.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 18:18:35 by azaragoz          #+#    #+#             */
+/*   Updated: 2025/07/23 18:38:10 by azaragoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
+/* int main(void)
+{
+	char	str1[] = "hoLa mUndo!12@;aab";
+
+	ft_strupcase(str1);
+	printf("'%s'\n", str1);
+}
+ */
